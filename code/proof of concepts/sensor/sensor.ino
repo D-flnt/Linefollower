754bcb1f5044e6e66bc0bffd54b7ca89e67c37c9
D-flnt <daan.flinterman@student.hogent.be>
2024-12-16 17:44:33 +0100

    Update sensor.ino

--- code/proof of concepts/sensor/sensor.ino
+++ code/proof of concepts/sensor/sensor.ino
@@ -5,24 +5,24 @@ int sensorValue4;
 int sensorValue5;
 int sensorValue6;
 
-const int sensorPin1 = A0;
-const int sensorPin2 = A1;
-const int sensorPin3 = A2;
-const int sensorPin4 = A3;
-const int sensorPin5 = A4;
-const int sensorPin6 = A5;
+const int sensorPin1 = 36;
+const int sensorPin2 = 39;
+const int sensorPin3 = 34;
+const int sensorPin4 = 35;
+const int sensorPin5 = 32;
+const int sensorPin6 = 33;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  sensorValue1 = map(analogRead(sensorPin1), 0, 1023, 1023, 0);
-  sensorValue2 = map(analogRead(sensorPin2), 0, 1023, 1023, 0);
-  sensorValue3 = map(analogRead(sensorPin3), 0, 1023, 1023, 0);
-  sensorValue4 = map(analogRead(sensorPin4), 0, 1023, 1023, 0);
-  sensorValue5 = map(analogRead(sensorPin5), 0, 1023, 1023, 0);
-  sensorValue6 = map(analogRead(sensorPin6), 0, 1023, 1023, 0);
+  sensorValue1 = map(analogRead(sensorPin1), 0, 4095, 4095, 0);
+  sensorValue2 = map(analogRead(sensorPin2), 0, 4095, 4095, 0);
+  sensorValue3 = map(analogRead(sensorPin3), 0, 4095, 4095, 0);
+  sensorValue4 = map(analogRead(sensorPin4), 0, 4095, 4095, 0);
+  sensorValue5 = map(analogRead(sensorPin5), 0, 4095, 4095, 0);
+  sensorValue6 = map(analogRead(sensorPin6), 0, 4095, 4095, 0);
 
   Serial.print("Sensorwaarden: ");
   Serial.print(sensorValue1); Serial.print(" ");
