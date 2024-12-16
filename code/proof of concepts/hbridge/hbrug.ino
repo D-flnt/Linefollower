b50e171f2de8691dce6f8f6d2d34a4aa493c4b6e
D-flnt <daan.flinterman@student.hogent.be>
2024-12-16 17:48:33 +0100

    hbrug.ino

--- code/proof of concepts/hbridge/hbrug.ino
+++ code/proof of concepts/hbridge/hbrug.ino
@@ -1,37 +1,28 @@
-//POC pwm sturing motoren (aparte snelheids regeling)
-
-const int Motorleft= 3;
-const int Motorright= 5;
+int motorA_IN1 = 4;
+int motorA_IN2 = 2;
+int motorB_IN1 = 19;
+int motorB_IN2 = 21;
 
 void setup() {
-Serial.begin(9600);
-pinMode(Motorleft, OUTPUT);
-pinMode(Motorright, OUTPUT);
-
+  pinMode(motorA_IN1, OUTPUT);
+  pinMode(motorA_IN2, OUTPUT);
+  pinMode(motorB_IN1, OUTPUT);
+  pinMode(motorB_IN2, OUTPUT);
 }
 
 void loop() {
-
-
-for (int i = 0; i <= 255; i+=5)
-{
-  analogWrite(Motorleft, i);
-  delay(30);
-}
-for (int i = 0; i <= 255; i+=5)
-{
-  analogWrite(Motorright, i);
-  delay(30);
-}
-
-for (int i = 255; i >= 0; i -= 5) 
-{
-  analogWrite(Motorleft, i);
-  delay(30);
-}
-for (int i = 255; i >= 0; i -= 5) 
-{
-  analogWrite(Motorright, i);
-  delay(30);
-}
+  for (int i = 0; i <= 255; i += 5) {
+    analogWrite(motorA_IN1, i);
+    analogWrite(motorA_IN2, 0);
+    analogWrite(motorB_IN1, i);
+    analogWrite(motorB_IN2, 0);
+    delay(30);
+  }
+  for (int i = 255; i >= 0; i -= 5) {
+    analogWrite(motorA_IN1, 0);
+    analogWrite(motorA_IN2, i);
+    analogWrite(motorB_IN1, 0);
+    analogWrite(motorB_IN2, i);
+    delay(30);
+  }
 }
